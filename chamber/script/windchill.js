25b9da89c998924c1d1efec113d263cc792ee865
yaqman <kokiyaq57@byui.edu>
2022-11-12 23:02:30 +0900

    week9

--- chamber/script/windchill.js
+++ chamber/script/windchill.js
@@ -1,20 +1,16 @@
-function windchill() {
+function chill() {
 
     var temp = document.getElementById('temp').textContent;
     var mph = document.getElementById('mph').textContent;
 
-    var chill = Chill(temp, mph);
-    document.getElementById("windchil").innerHTML = chill;
-  }
-
-  function Chill(temp, mph) {
     var windchill;
-    if (temp < 75 && mph > 3) {
-    windchill = 35.74 + 0.6215*(temp) - 35.75*(Math.pow(mph, 0.16)) + 0.4275*(temp)*(Math.pow(mph, 0.16));
-    windchill = windchill.toFixed(0);
-    } else {
-      windchill = "(N/A)";
-    }
 
-    return windchill;
+    windchill = 35.74 + 0.6215*(temp) - 35.75*(Math.pow(mph, 0.16)) + 0.4275*(temp)*(Math.pow(mph, 0.16));
+
+    let n = windchill.toFixed(1);
+
+    document.getElementById('windchill').innerHTML = n; 
+
 }
+
+chill();
