e2780bc1250fac16b576f9d0dd1da802cbae23d2
yaqman <kokiyaq57@byui.edu>
2022-11-18 13:07:48 +0900

    week10

---
+++ week10/weatherapi.js
@@ -0,0 +1,41 @@
+const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&appid=c99481515e9cb2b9ee33aaec3bad8994&units=imperial'
+
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+  apiFetch();
+
+function capFirstLetter(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function  displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+  
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+  }
+
+
+requestData(url);
